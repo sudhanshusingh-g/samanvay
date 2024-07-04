162bd8719dfd85ec842f241ca62138535a431f76
sudhanshusingh-g <sudsingh32@gmail.com>
2024-07-04 13:27:04 +0530

    Board page continued.

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -5,6 +5,7 @@ import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import Link from "next/link"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export const BoardList=async()=>{
   const {orgId}=auth();
@@ -59,4 +60,19 @@ export const BoardList=async()=>{
         </div>
       </div>
     );
+}
+
+BoardList.Skeleton=function SkeletonBoardList(){
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+    </div>
+  );
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,14 @@
+import { auth } from "@clerk/nextjs/server";
 import OrgControl from "./_components/org-control";
+import {startCase} from "lodash";
 
+export async function generateMetaData(){
+  const {orgSlug}=auth();
+
+  return {
+    title:startCase(orgSlug || "organization"),
+  };
+}
 
 function OrganizationIdLayout({ children }: { children: React.ReactNode }) {
   return <>
