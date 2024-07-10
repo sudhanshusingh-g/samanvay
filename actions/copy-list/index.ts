fb9c4256afa133c218b8e6536298cad69c2e9794
sudhanshusingh-g <sudsingh32@gmail.com>
2024-07-10 14:08:50 +0530

    List Options

---
+++ actions/copy-list/index.ts
@@ -0,0 +1,73 @@
+"use server";
+import { auth } from "@clerk/nextjs/server";
+import { InputType,ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CopyList } from "./schema";
+
+const handler=async (data:InputType):Promise<ReturnType>=>{
+    const {userId,orgId}=auth();
+    if(!userId || !orgId){
+        return{
+            error:"Unautorized",
+        };
+    }
+
+    const {id,boardId}=data;
+    let list;
+    try {
+        const listCopy=await db.list.findUnique({
+            where:{
+                id,
+                boardId,
+                board:{
+                    orgId
+                }
+            },
+            include:{
+                cards:true
+            }
+        })
+        if(!listCopy){
+            return{error:"List not found"}
+        }
+
+        const lastList=await db.list.findFirst({
+            where:{boardId},
+            orderBy:{order:"desc"},
+            select:{order:true}
+        })
+
+        const newOrder=lastList ? lastList.order+1:1;
+
+        list=await db.list.create({
+            data:{
+                boardId:listCopy.boardId,
+                title:`${listCopy.title} -Copy`,
+                order:newOrder,
+                cards:{
+                    createMany:{
+                        data:listCopy.cards.map((card)=>({
+                            title:card.title,
+                            description:card.description,
+                            order:card.order
+                        }))
+                    }
+                }
+            },
+            include:{
+                cards:true
+            }
+            
+        })
+    } catch (error) {
+        return{
+            error:"Failed to copy."
+        }
+    }
+    revalidatePath(`/board/${boardId}`);
+    return {data:list};
+}
+
+export const copyList=createSafeAction(CopyList,handler);
