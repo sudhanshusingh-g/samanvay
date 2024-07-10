c506d401ce1851e9c3340f13eaf127f1d7ffa6e2
sudhanshusingh-g <sudsingh32@gmail.com>
2024-07-10 12:51:34 +0530

    List Headers

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps{
     data:ListWithCards[];
@@ -9,8 +11,21 @@ interface ListContainerProps{
 }
 
 export const ListContainer=({data,boardId}:ListContainerProps)=>{
+    const [orderedData,setOrderedData]=useState(data);
+    useEffect(()=>{
+        setOrderedData(data);
+    },[data])
     return(
-        <ol className="mt-4">
+        <ol className="mt-4 flex gap-x-3 h-full">
+            {
+                orderedData.map((list,index)=>(
+                    <ListItem
+                    key={list.id}
+                    index={index}
+                    data={list}
+                    />
+                ))
+            }
             <ListForm/>
             <div
             className="flex-shrink-0 w-1"
