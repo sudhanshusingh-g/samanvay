58d7aea2420996b6bb9c1362ab0948ef696a1865
sudhanshusingh-g <sudsingh32@gmail.com>
2024-07-17 11:11:35 +0530

    deployment

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,45 +4,61 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export async function POST(req:Request){
- const body=await req.text();
- const signature=headers().get("Stripe-Signature") as string;
- 
- let event:Stripe.Event;
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 
- try {
-    event=stripe.webhooks.constructEvent(
-        body,
-        signature,
-        process.env.STRIPE_WEBHOOK_SECRET!,
-    )
- } catch (error) {
-    return new NextResponse("Webhook error",{status:400});
- }
+  let event: Stripe.Event;
 
- const session=event.data.object as Stripe.Checkout.Session;
-
- if(event.type === "checkout.session.completed"){
-    const subscription=await stripe.subscriptions.retrieve(
-        session.subscription as string
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
     );
-    if(!session?.metadata?.orgId){
-        return new NextResponse("Org Id is required",{status:400});
+  } catch (error) {
+    return new NextResponse("Webhook error", { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+    if (!session?.metadata?.orgId) {
+      return new NextResponse("Org Id is required", { status: 400 });
     }
     await db.orgSubscription.create({
-    data:{
-        orgId:session?.metadata?.orgId,
-        stripeSubscriptionId:subscription.id,
-        stripeCustomerId:subscription.customer as string,
-        stripePriceId:subscription.items.data[0].price.id,
+      data: {
+        orgId: session?.metadata?.orgId,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
 
-    }
- })
- }
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
 
- 
+    await db.orgSubscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
 
-
-
-
+  return new NextResponse(null,{status:200});
 }
